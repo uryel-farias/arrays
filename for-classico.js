1767187d5e18981c5293695d554ac465388f06ad
Uryel Farias <uryel.farias@gmail.com>
2025-05-02 12:45:53 -0300

    Percorrendo um array com for e calculando a media

--- for-classico.js
+++ for-classico.js
@@ -1,5 +1,10 @@
-const notas = [10, 8.5, 5, 6.5, 8, 7.5];
+const notas = [10, 6.5, 8, 7.5];
 
-for (let indice = 0; indice < notas.length; indice++) {
-    console.log(indice, notas[indice]);
+let somaDasNotas = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    somaDasNotas += notas[i];
 }
+
+const media = somaDasNotas / notas.length;
+console.log(`a média das notas é ${media}`);
