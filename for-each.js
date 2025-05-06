0fafc501eb421cfe2f4b651c0045de699110c8e4
Uryel Farias <uryel.farias@gmail.com>
2025-05-05 23:35:06 -0300

    removendo parametros repetidos

--- for-each.js
+++ for-each.js
@@ -2,10 +2,6 @@ const notas = [10, 6.5, 8, 7.5];
 
 let somaDasNotas = 0;
 
-const notas = [10, 6.5, 8, 7.5];
-
-let somaDasNotas = 0;
-
 notas.forEach()(function (nota) {
     somaDasNotas += nota;
 })
