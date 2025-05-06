4db5eada5cbdf227fdb7725cd7af71737efe643a
Uryel Farias <uryel.farias@gmail.com>
2025-05-06 09:48:22 -0300

    usando o map e arrow function

--- for-each.js
+++ for-each.js
@@ -2,10 +2,9 @@ const notas = [10, 6.5, 8, 7.5];
 
 let somaDasNotas = 0;
 
-notas.forEach()(function (nota) {
+notas.forEach(somaNotas);
+function somaNotas (nota) {
     somaDasNotas += nota;
-})
-
+}
 const media = somaDasNotas / notas.length;
-
 console.log(`a média das notas é ${media}`);
